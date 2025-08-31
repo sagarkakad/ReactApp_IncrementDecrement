145d1db3058b51c19e4c41255da71f0b4defa21b
sagarkakad <snkakad@gmail.com>
2025-08-31 12:22:48 +0530

    app name change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ClassComponent from './Component/ClassComponent';
 function App() {
   return (
    <div className="app-container">
-      <h1 className="main-heading">Assignment-2 To Do App</h1>
+      <h1 className="main-heading">Assignment-2 Counter Application</h1>
       <div className="row">
         <ClassComponent />
         <FunctionalComponent />
